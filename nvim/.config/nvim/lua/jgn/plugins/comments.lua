6b049b2931293ba6ae955f8b3775d198d5c8c01f
Jeremy Novak <jgn@jgn.dev>
2024-08-11 12:26:35 -0500

    reset neovim

---
+++ nvim/.config/nvim/lua/jgn/plugins/comments.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "numToStr/Comment.nvim",
+    lazy = false,
+    config = function()
+      require("Comment").setup {}
+    end,
+  },
+
+  {
+    "folke/todo-comments.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = { signs = false },
+  },
+}
