cd10ba669f4db065089b5cbe78f77aab9697ff82
Jeremy Novak <jgn@jgn.dev>
2024-08-28 22:44:03 -0500

    changed theme to catppuccin

--- nvim/.config/nvim/lua/jgn/plugins/colors.lua
+++ nvim/.config/nvim/lua/jgn/plugins/colors.lua
@@ -1,29 +1,29 @@
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("kanagawa").setup {
-        colors = {
-          palette = {},
-          theme = {
-            all = {
-              ui = {
-                bg_gutter = "none",
-              },
-            },
-          },
-        },
-      }
-      vim.cmd.colorscheme "kanagawa-wave"
-    end,
-  },
-
   -- {
-  --   "catppuccin/nvim",
+  --   "rebelot/kanagawa.nvim",
+  --   lazy = false,
+  --   priority = 1000,
   --   config = function()
-  --     vim.cmd.colorscheme "catppuccin-mocha"
+  --     require("kanagawa").setup {
+  --       colors = {
+  --         palette = {},
+  --         theme = {
+  --           all = {
+  --             ui = {
+  --               bg_gutter = "none",
+  --             },
+  --           },
+  --         },
+  --       },
+  --     }
+  --     vim.cmd.colorscheme "kanagawa-wave"
   --   end,
   -- },
+
+  {
+    "catppuccin/nvim",
+    config = function()
+      vim.cmd.colorscheme "catppuccin-mocha"
+    end,
+  },
 }
