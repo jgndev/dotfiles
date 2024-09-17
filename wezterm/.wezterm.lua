f6fa12769d0d5734a5f7a97512442cbecf14a493
Jeremy Novak <jgn@jgn.dev>
2024-09-17 15:17:36 -0500

    updated dotfiles

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,34 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- Font related
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+-- config.font = wezterm.font("Hack Nerd Font Mono")
+config.font_size = 16.0
+-- config.dpi = 144.0
+config.freetype_load_flags = "NO_HINTING"
+
+-- Colorscheme
+-- config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Gruvbox dark, hard (base16)"
+config.color_scheme = "kanagawabones"
+-- config.color_scheme = "GruvboxDarkHard"
+
+-- Window Decorations
+config.window_decorations = "RESIZE"
+
+-- GPU Accelerated
+config.front_end = "WebGpu"
+
+-- Disable update notifications
+config.check_for_updates = false
+config.show_update_window = false
+
+return config
