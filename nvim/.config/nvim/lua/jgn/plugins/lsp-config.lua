583673761c4f1425f9b6977ecb5b47831a604092
Jeremy Novak <jgn@jgn.dev>
2024-10-02 14:59:34 -0500

    updating dotfiles

--- nvim/.config/nvim/lua/jgn/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/jgn/plugins/lsp-config.lua
@@ -130,84 +130,11 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         bashls = {},
-        clangd = {
-          keys = {
-            { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
-          },
-          root_dir = function(fname)
-            return require("lspconfig.util").root_pattern(
-              "Makefile",
-              "configure.ac",
-              "configure.in",
-              "config.h.in",
-              "meson.build",
-              "meson_options.txt",
-              "build.ninja"
-            )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(fname) or require("lspconfig.util").find_git_ancestor(
-              fname
-            )
-          end,
-          capabilities = {
-            offsetEncoding = { "utf-16" },
-          },
-          cmd = {
-            "clangd",
-            "--background-index",
-            "--clang-tidy",
-            "--header-insertion=iwyu",
-            "--completion-style=detailed",
-            "--function-arg-placeholders",
-            "--fallback-style=llvm",
-          },
-          init_options = {
-            usePlaceholders = true,
-            completeUnimported = true,
-            clangdFileStatus = true,
-          },
-        },
         cssls = {},
         docker_compose_language_service = {},
         dockerls = {},
         emmet_ls = {},
         eslint = {},
-        -- gopls = {
-        --   settings = {
-        --     gofumpt = true,
-        --     codelenses = {
-        --       gc_details = false,
-        --       generate = true,
-        --       regenerate_cgo = true,
-        --       run_govulncheck = true,
-        --       test = true,
-        --       tidy = true,
-        --       upgrade_dependency = true,
-        --       vendor = true,
-        --     },
-        --     hints = {
-        --       assignVariableTypes = true,
-        --       compositeLiteralFields = true,
-        --       compositeLiteralTypes = true,
-        --       constantValues = true,
-        --       functionTypeParameters = true,
-        --       parameterNames = true,
-        --       rangeVariableTypes = true,
-        --     },
-        --     analyses = {
-        --       fieldalignment = true,
-        --       nilness = true,
-        --       unusedparams = true,
-        --       unusedwrite = true,
-        --       useany = true,
-        --     },
-        --     usePlaceholders = true,
-        --     completeUnimported = true,
-        --     staticcheck = true,
-        --     directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-        --     semanticTokens = true,
-        --   },
-        -- },
-        -- goimports = {},
-        -- gofumpt = {},
         hydra_lsp = {},
         jsonls = {},
         marksman = {},
