49b73a57787a9591fe8931c8bfda33d723ca15b6
Jeremy Novak <jgn@jgn.dev>
2025-11-16 10:02:22 -0600

    working on custom nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -474,6 +474,41 @@ require('lazy').setup({
     },
   },
 
+  -- Oil.nvim - Edit filesystem like a buffer
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    keys = {
+      { '<leader>E', '<cmd>Oil<cr>', desc = 'Open file [E]xplorer' },
+    },
+    opts = {
+      default_file_explorer = true,
+      columns = {
+        'icon',
+      },
+      view_options = {
+        show_hidden = true,
+      },
+      keymaps = {
+        ['g?'] = 'actions.show_help',
+        ['<CR>'] = 'actions.select',
+        ['<C-v>'] = 'actions.select_vsplit',
+        ['<C-x>'] = 'actions.select_split',
+        ['<C-t>'] = 'actions.select_tab',
+        ['<C-p>'] = 'actions.preview',
+        ['<C-c>'] = 'actions.close',
+        ['<C-r>'] = 'actions.refresh',
+        ['-'] = 'actions.parent',
+        ['_'] = 'actions.open_cwd',
+        ['`'] = 'actions.cd',
+        ['~'] = 'actions.tcd',
+        ['gs'] = 'actions.change_sort',
+        ['gx'] = 'actions.open_external',
+        ['g.'] = 'actions.toggle_hidden',
+      },
+    },
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
